6b4fa7a19f29c8b5c744434e8aeff902bd967a50
Pootisspencerq <140479292+Pootisspencerq@users.noreply.github.com>
2023-08-31 16:30:38 +0300

    Add files via upload

--- logika/m2/memory_card/memo___card_layout.py
+++ logika/m2/memory_card/memo___card_layout.py
@@ -9,18 +9,78 @@ from PyQt5.QtWidgets import (
         QPushButton, QLabel, QSpinBox)
 app = QApplication([])
 
-# віджети, які треба буде розмістити:
-# кнопка повернення в основне вікно 
-# кнопка прибирає вікно і повертає його після закінчення таймера
-# введення кількості хвилин
-# кнопка відповіді "Ок" / "Наступний"
-# текст питання
+btn_menu = QPushButton('Меню')
+btn_sleep = QPushButton('Відпочити')
+btn_OK = QPushButton('Відповісти')
+lb_Question = QLabel('')
 
-# Опиши групу перемикачів
+box_minutes = QSpinBox()
+box_minutes.setValue(5)
 
-# Опиши панень з результатами
+RadioGroupBox = QGroupBox('Варіанти відповідей')
+RadioGroup = QButtonGroup()
 
-# Розмісти весь вміст в лейаути. Найбільшим лейаутом буде layout_card
+rbtn_1 = QRadioButton('')
+rbtn_2 = QRadioButton('')
+rbtn_3 = QRadioButton('')
+rbtn_4 = QRadioButton('')
+
+RadioGroup.addButton(rbtn_1)
+RadioGroup.addButton(rbtn_2)
+RadioGroup.addButton(rbtn_3)
+RadioGroup.addButton(rbtn_4)
+
+layout_ans1 = QHBoxLayout()
+layout_ans2 = QVBoxLayout()
+layout_ans3 = QVBoxLayout()
+
+layout_ans2.addWidget(rbtn_1)
+layout_ans2.addWidget(rbtn_2)
+layout_ans3.addWidget(rbtn_3)
+layout_ans3.addWidget(rbtn_4)
+
+layout_ans1.addLayout(layout_ans2)
+layout_ans1.addLayout(layout_ans3)
+
+RadioGroupBox.setLayout(layout_ans1)
+
+AnsGroupBox = QGroupBox("Результат тесту")
+lb_Result = QLabel('') 
+lb_Correct = QLabel('') 
+
+layout_res = QVBoxLayout()
+layout_res.addWidget(lb_Result, alignment=(Qt.AlignLeft | Qt.AlignTop))
+layout_res.addWidget(lb_Correct, alignment=Qt.AlignHCenter)
+AnsGroupBox.setLayout(layout_res)
+AnsGroupBox.hide()
+
+layout_line1 = QHBoxLayout()
+layout_line2 = QHBoxLayout()
+layout_line3 = QHBoxLayout()
+layout_line4 = QHBoxLayout()
+
+layout_line1.addWidget(btn_menu)
+layout_line1.addStretch(1)
+layout_line1.addWidget(btn_sleep)
+layout_line1.addWidget(box_minutes)
+layout_line1.addWidget(QLabel('хвилин'))
+
+layout_line2.addWidget(lb_Question, alignment=(Qt.AlignHCenter | Qt.AlignVCenter))
+
+layout_line3.addWidget(RadioGroupBox)
+layout_line3.addWidget(AnsGroupBox)
+
+layout_line4.addStretch(1)
+layout_line4.addWidget(btn_OK)
+layout_line4.addStretch(1)
+layout_card = QVBoxLayout()
+layout_card.addLayout(layout_line1, stretch=1)
+layout_card.addLayout(layout_line2, stretch=2)
+layout_card.addLayout(layout_line3, stretch=8)
+layout_card.addStretch(1)
+layout_card.addLayout(layout_line4, stretch=1)
+layout_card.addStretch(1)
+layout_card.setSpacing(5)
 
 # Результат роботи цього модуля: віджети поміщені всередину layout_card, який можна призначити вікну.
 def show_result():
--- logika/m2/memory_card/memo___main.py
+++ logika/m2/memory_card/memo___main.py
@@ -1,8 +1,14 @@
 from memo___card_layout import *
 from PyQt5.QtWidgets import QWidget
 from random import shuffle # будемо змішувати відповіді в картці питання
-app = QApplication([])
+from memo____data import *
+
 card_width, card_height = 600, 500 # початкові розміри вікна "картка"
+
+radio_list = [rbtn_1, rbtn_2, rbtn_3, rbtn_4]
+
+frm = Question('Яблуко', 'Apple', 'Caterpillar', 'house', 'cat')
+frm_card = QuestionView(frm, lb_Question, radio_list[0], radio_list[1], radio_list[2], radio_list[3])
 
 def show_data():
     ''' показує на екрані потрібну інформацію '''
@@ -15,7 +21,11 @@ def check_result():
     pass
 
 win_card = QWidget()
+win_card.resize(card_width, card_height)
 #тут повинні бути параметри вікна
 
+frm_card.show()
+
+win_card.setLayout(layout_card)
 win_card.show()
 app.exec_()

7c9d9b4f79865e8dbbde42938e2959d26c1e1759
Pootisspencerq <140479292+Pootisspencerq@users.noreply.github.com>
2023-10-26 20:09:26 +0300

    Add files via upload

--- logika/m4/easy_editor/main.py
+++ logika/m4/easy_editor/main.py
@@ -44,11 +44,7 @@ col2.addLayout(row)
 layot_editor.addLayout(col1, 1)
 layot_editor.addLayout(col2, 4)
 
-workdir = QFileDialog.getExistingDirectory()
-print(workdir)
 
-files =os.listdir(workdir)
-print(files)
 
 def filter(filename):
     result = []
@@ -57,8 +53,57 @@ def filter(filename):
         if file.split('.')[-1] in ext:
             result.append(file)
     return result        
+
+def show_files():
+    global workdir
+    workdir = QFileDialog.getExistingDirectory()
+    files =os.listdir(workdir)
+    graphic_files = filter(files)
+    
+    lst_files.clear()
+    lst_files.addItems(graphic_files)
+    
+class ImageProcessor():
+    def __init__(self):
+        self.original = None
+        self.filename = None
+        self.save_dir = 'Modified/'
+    def load_image(self, filename):
+        self.filename = filename
+        full_path = os.path.join(workdir, filename)
+        self.original =Image.open(full_path)
+     
+    def show_Image(self, path):
+        lb_pic.hide()
+    
+        pixmapimage =QPixmap(path)
         
-print(filter(files))
+        w, h = lb_pic.width(), lb_pic.height()
+        pixmapimage =pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+        
+        lb_pic.setPixmap(pixmapimage) 
+        lb_pic.show() 
+        
+    def saveandShow(self):
+        path = os.path.join(workdir, self.save_dir)  
+        if not(os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+            
+        image_path = os.path.join(path, self.filename)   
+        self.original.save(image_path)
+        self.show_Image(image_path)
+         
+def showChosenItem():
+    filename = lst_files.currentItem().text()
+    workimage.load_image(filename)
+    full_path = os.path.join(workdir, filename)
+    workimage.show_Image(full_path)   
+workimage = ImageProcessor()  
+    
+lst_files.currentRowChanged.connect(showChosenItem)
+btn_folder.clicked.connect(show_files) 
+        
+
 window.setStyleSheet('''
                         background-color: rgb(0,0,255); 
                         color: yellow;

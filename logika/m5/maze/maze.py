257768f2454e43c93e8040e49ad8c2b4c49d9afa
Pootisspencerq <140479292+Pootisspencerq@users.noreply.github.com>
2023-11-30 20:03:02 +0200

    Add files via upload

--- logika/m5/maze/maze.py
+++ logika/m5/maze/maze.py
@@ -44,6 +44,23 @@ class Enemy(Gamesprite):
             self.direction = 'right'  
         if  self.rect.x >=win_width -80:
             self.direction = 'left'  
+            
+            
+class Wall(sprite.Sprite):
+    def __init__(self, wall_x, wall_y, wall_widht, wall_height ):
+        super().__init__()
+        self.widht= wall_widht
+        self.height= wall_height
+                  
+        
+        self.image = Surface((self.widht, self.height)) 
+        self.image.fill((0, 225, 0)) 
+        
+        self.rect = self.image.get_rect()
+        self.rect.x =wall_x
+        self.rect.y =wall_y
+    def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))          
 win_width = 700
 win_height=500
 
@@ -53,17 +70,28 @@ background =scale(image.load('background.jpg'), (win_width, win_height))
 player = Player("hero.png", 5, win_height - 80, 4)
 evil = Enemy("cyborg.png", win_width - 115, win_height - 280, 3 )
 treasure =Gamesprite('treasure.png', win_width-80,win_height-80, 0)
+wall=Wall(10, 10, 200, 100)
+wall2=Wall(15, 10, 100, 200)
+
+walls = [wall, wall2]
+
 
 clock = time.Clock()
 FPS = 60
 game =1
 finish = 0
 
+font.init()
+f = font.Font(None, 70)
+
+win = f.render('YOURE A WINNER!', True, (225, 215, 0))
+lose = f.render('YOURE A LOSER!', True, (225, 0, 0))
 mixer.init()
 mixer_music.load("jungles.ogg")
 mixer_music.play()
 
-
+money_sound = mixer.Sound('money.ogg')
+kick_sound = mixer.Sound('kick.ogg')
 while game:
     for e in event.get():
         if e.type ==QUIT:
@@ -74,8 +102,24 @@ while game:
         player.reset()
         evil.reset()
         treasure.reset()
+        wall.reset()
+        wall2.reset()
+        
         player.update()
         evil.update()
-    
+        if sprite.collide_rect(player, treasure):
+            finish = 1
+            window.blit(win, (200, 200))
+            money_sound.play()
+            
+        if sprite.collide_rect(player, evil) or sprite.collide_rect(player, wall) :
+            finish = 1
+            window.blit(lose, (200, 200))
+            money_sound.play() 
+        for wall in walls:
+            if sprite.collide_rect(player, wall):
+                finish = True
+                window.blit(lose, (200, 200))
+                kick_sound.play()
     display.update()
     clock.tick(FPS)
